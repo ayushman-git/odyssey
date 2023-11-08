f783047177975f57b5bf1eb46aa4b3710a162b1a
Ayushman <ayushman1998.1998@gmail.com>
2023-11-08 15:36:46 +0530

    using metadata to populate article page

--- src/app/articles/[slug]/page.jsx
+++ src/app/articles/[slug]/page.jsx
@@ -1,10 +1,26 @@
 import { getArticle } from "@/lib/posts";
+import Image from "next/image";
 
 export default async function Page({ params }) {
-  const postData = await getArticle(params.slug);
+  const { htmlContent, title, cover_img, type } = await getArticle(params.slug);
   return (
-    <article dangerouslySetInnerHTML={{__html: postData.htmlContent}}>
-
-    </article>
+    <section className="py-8">
+      <Image
+        src={cover_img}
+        width={800}
+        height={300}
+        style={{
+          aspectRatio: "2/1",
+          objectFit: "cover",
+        }}
+        layout="responsive"
+        objectFit="cover"
+        className="mr-10 rounded"
+        alt="Cover Image"
+      />
+      <h1 className="text-2xl font-black mt-4">{title}</h1>
+      <h2 className="font-black mt-2">{type}</h2>
+      <article dangerouslySetInnerHTML={{ __html: htmlContent }}></article>
+    </section>
   );
 }
