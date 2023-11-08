0f137f81d7cfc8082341d6863435fdf71a584ffd
Ayushman <ayushman1998.1998@gmail.com>
2023-11-08 18:33:48 +0530

    fixed cover image on article page

--- src/app/articles/[slug]/page.jsx
+++ src/app/articles/[slug]/page.jsx
@@ -4,17 +4,20 @@ import Image from "next/image";
 export default async function Page({ params }) {
   const { htmlContent, title, cover_img, type } = await getArticle(params.slug);
   return (
-    <section className="py-8">
-      <Image
-        src={cover_img}
-        quality={100}
-        width={800}
-        height={300}
-        layout="responsive"
-        objectFit="cover"
-        className="mr-10 rounded"
-        alt="Cover Image"
-      />
+    <section className="py-8 relative">
+      <div className="w-full h-96 relative">
+        <Image
+          src={cover_img}
+          quality={100}
+          style={{
+            borderRadius: "14px",
+          }}
+          layout="fill"
+          objectFit="cover"
+          className="mr-10 rounded"
+          alt="Cover Image"
+        />
+      </div>
       <h1 className="text-2xl font-black mt-4">{title}</h1>
       <h2 className="font-black mt-2">{type}</h2>
       <article dangerouslySetInnerHTML={{ __html: htmlContent }}></article>
