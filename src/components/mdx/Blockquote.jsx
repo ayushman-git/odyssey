62020b993d5d2adfc3b425c7c804c402455b1882
Ayushman <ayushman1998.1998@gmail.com>
2023-11-10 13:06:38 +0530

    removed log

--- src/components/mdx/Blockquote.jsx
+++ src/components/mdx/Blockquote.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Blockquote({ children }) {
-    console.log({children})
   return (
     <blockquote className="border-l-2 border-green-600 bg-slate-200 italic">
       {children}
