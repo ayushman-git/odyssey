6d6b1ed28c916210c6b0ce889c0b6bfc4394e13d
Ayushman <ayushman1998.1998@gmail.com>
2023-11-11 12:51:41 +0530

    added a link icon on heading hover

--- src/components/mdx/Heading.jsx
+++ src/components/mdx/Heading.jsx
@@ -1,13 +1,15 @@
 import { underscoreDelimiter } from "@/utils";
+import { BiLink } from "react-icons/bi";
 
 export const Heading = {
   H1: ({ children }) => {
     return (
       <h1
-        className="text-2xl font-bold my-4 cursor-pointer"
+        className="text-2xl font-bold my-4 cursor-pointer flex items-center gap-2 group"
         id={underscoreDelimiter(children)}
       >
         <a href={`#${underscoreDelimiter(children)}`}>{children}</a>
+        <BiLink className="opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
       </h1>
     );
   },
