f7a23a7365cdc3393d2209b4d8ad454598cf4ea9
Ayushman <ayushman1998.1998@gmail.com>
2023-11-20 12:46:37 +0530

    made aside menu dynamic, added intersection observer

--- src/components/AsideTitles.jsx
+++ src/components/AsideTitles.jsx
@@ -1,15 +1,44 @@
 "use client";
 
 import { underscoreDelimiter } from "@/utils";
+import { Fragment, useEffect, useState } from "react";
 
 export default function AsideTitles({ headings }) {
+  const [currentSectionInView, setCurrentSectionInView] = useState(null);
+
+  const handleIntersection = (entries, observer, id) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("IN", id);
+        setCurrentSectionInView(id);
+      }
+    });
+  };
+
+  useEffect(() => {
+    headings.forEach(({ title }) => {
+      const id = underscoreDelimiter(title);
+      const headingElement = document.getElementById(id);
+      const headingWrapperElement = headingElement.parentElement;
+
+      const observer = new IntersectionObserver(
+        (entries, observer) => handleIntersection(entries, observer, id),
+        {
+          threshold: 0.5,
+        }
+      );
+
+      observer.observe(headingWrapperElement);
+    });
+  }, []);
+
   const renderAsideList = (list) => {
     return list.map((item) => {
       return (
-        <>
+        <Fragment key={item.title}>
           {item.title && (
             <a
-              className="text-gray-500 "
+              className={currentSectionInView === underscoreDelimiter(item.title) ? `text-blue-500 font-bold` : `text-gray-500`}
               href={`#${underscoreDelimiter(item.title)}`}
             >
               <li className="my-3">{item.title}</li>
@@ -18,15 +47,15 @@ export default function AsideTitles({ headings }) {
           {item.subheadings &&
             item.subheadings.length > 0 &&
             renderAsideList(item.subheadings)}
-        </>
+        </Fragment>
       );
     });
   };
 
   return (
     <aside className="sticky top-10 w-60 -ml-72 h-0 bottom-24">
-        <h3 className="font-black mb-3">Table of Contents</h3>
-        <hr className="mb-6" />
+      <h3 className="font-black mb-3">Table of Contents</h3>
+      <hr className="mb-6" />
       <ul className="text-sm">{renderAsideList(headings)}</ul>
     </aside>
   );
