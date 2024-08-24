4c029e1079c7dea22f9c76570d7fa6974830f679
xenon <ayushman.gupta308@gmail.com>
2024-08-24 20:59:14 +0530

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,30 @@
 "use client";
 
 import profiles from "@/assets/json/profiles.json";
-import { openLink } from "@/utils";
-import { FiExternalLink } from "react-icons/fi";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-black text-white grid place-items-center py-24 rounded-t-3xl relative z-10">
-      <div className="max-w-screen-md w-full flex gap-10">
-        <aside className="w-full">
-          <h1 className="text-lg font-black">Odyssey</h1>
-          <h3 className="opacity-60 text-sm mt-2">
-            A Voyage into the Heart of Silicon
-          </h3>
-        </aside>
+    <footer className="max-w-screen-md w-full p-24 pb-8 bg-black text-white grid place-items-center py-24 rounded-[40px] mb-12 relative z-10">
+      <h1 className="text-5xl font-bold text-center leading-normal">
+        Dream big, stay curious, keep learning
+      </h1>
+      <div className="max-w-screen-md w-full flex gap-10 mt-12">
         <section className="w-full">
-          <h3 className="opacity-60 text-sm mb-4">Find me on</h3>
-          <ul className="flex gap-10 mb-8">
+          <ul className="flex items-center justify-center gap-10">
             {profiles.map(({ name, link }) => (
-              <li
-                className="flex items-center gap-2 cursor-pointer"
-                onClick={() => openLink(link)}
-                key={name}
-              >
-                <a className="text-blue-500">{name}</a>
-                <FiExternalLink className="cursor-pointer text-blue-500" />
+              <li className="flex items-center gap-2 cursor-pointer" key={name}>
+                <a className="text-gray-400" target="_blank" href={link}>
+                  {name}
+                </a>
               </li>
             ))}
           </ul>
-          <h3 className="opacity-60 text-sm mb-4">Subscribe to newsletter</h3>
-          <div className="flex">
-            <input
-              placeholder="Enter email id"
-              className="border border-gray-300 bg-transparent p-4 rounded-xl w-full"
-              type="text"
-            />
-            <button className="bg-blue-600 p-4 px-10 rounded-xl ml-4">
-              Subscribe
-            </button>
-          </div>
         </section>
+      </div>
+      <div>
+        <p className="text-gray-700 mt-12">
+          &copy; {new Date().getFullYear()} Odyssey | Ayushman
+        </p>
       </div>
     </footer>
   );
