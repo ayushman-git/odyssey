b3250c17f09d22d8ec371ba85707beb4a037eb64
xenon <ayushman.gupta308@gmail.com>
2024-09-08 20:32:57 +0530

    added images

--- src/components/mdx/BaseElements/Figcaption.jsx
+++ src/components/mdx/BaseElements/Figcaption.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Figcaption({ children }) {
-  return <figcaption className="text-gray-700 text-sm text-center dark:text-gray-200 my-4">{children}</figcaption>;
+  return <figcaption className="text-gray-700 text-sm text-center dark:text-gray-400 my-4">{children}</figcaption>;
 }
 
 export default Figcaption;
