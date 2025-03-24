e5bc2909fafbb110e8e715861387e9a5dbd47f59
Ayushman Gupta <ayushman.gupta308@gmail.com>
2025-03-24 11:32:04 +0530

    removed ga path override

--- src/lib/GoogleAnalyticsProvider.jsx
+++ src/lib/GoogleAnalyticsProvider.jsx
@@ -17,7 +17,6 @@ const GoogleAnalytics = () => {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
               });
           `}
       </Script>
