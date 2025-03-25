616ebbdba51245743f7fa2b5925450f5e72f5d09
Ayushman Gupta <ayushman.gupta308@gmail.com>
2025-03-25 15:42:19 +0530

    fixed image caching

--- src/components/CreativeGrid/Grid.jsx
+++ src/components/CreativeGrid/Grid.jsx
@@ -35,7 +35,6 @@ function Grid({ img, gridArea, onLoadingComplete }) {
         <Image
           onLoadingComplete={onLoadingComplete}
           src={img}
-          placeholder="blur"
           alt="Cover Image"
           fill
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
